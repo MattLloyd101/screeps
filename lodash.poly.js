2937f3b3f47b6d80732eed5cacf14a1139b514b9
Matthew Lloyd <matt.lloyd101@gmail.com>
2017-09-24 21:24:00 +0100

    Matt - Wiping old work

--- lodash.poly.js
+++
@@ -1,6 +0,0 @@
-const _ = require("lodash");
-
-module.exports = _.extend(_, {
-  flatMap: _.compose(_.flatten, _.map),
-  isNil: (value) => _.isUndefined(value) || _.isNull(value)
-});
--- test/role.harvester.basic.behaviour.test.js
+++
@@ -1,9 +0,0 @@
-const sinon = require('sinon');
-const describe = require("mocha").describe;
-const it = require("mocha").it;
-const expect = require('chai').expect;
-
-describe('Basic Harvester Role - Behaviour', () => {
-
-
-});
